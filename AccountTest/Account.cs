20a28b8d13871a76d2518cf717d97a60baa6c7d0
Chance Smith <chancesmithb@gmail.com>
2020-01-08 18:52:40 -0600

    init commmit

---
+++ AccountTest/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountTest
+{
+    internal class Account
+    {
+        private string name; // instance variable
+
+        // method that set the account name in the object
+        public void SetName(string accountName)
+        {
+            name = accountName; // store the account name
+        }
+
+        // method that retrieves the account name from the object
+        public string GetName()
+        {
+            return name; // returns name's value to this method's caller
+        }
+    }
+}
---
+++ AccountTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountTest
+{
+    internal class AccountTest
+    {
+        private static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            myAccount.SetName("Chance-100");
+            Console.WriteLine($"You are viewing account: {myAccount.GetName()}");
+        }
+    }
+}
