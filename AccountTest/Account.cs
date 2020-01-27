f5c736bab149fe136bde8aa3b8eddbacfc4e9ce1
Chance Smith <chancesmithb@gmail.com>
2020-01-26 20:50:07 -0600

    adds Name property - long form

--- AccountTest/Account.cs
+++ AccountTest/Account.cs
@@ -8,16 +8,16 @@ namespace AccountTest
     {
         private string name; // instance variable
 
-        // method that set the account name in the object
-        public void SetName(string accountName)
+        public string Name
         {
-            name = accountName; // store the account name
-        }
-
-        // method that retrieves the account name from the object
-        public string GetName()
-        {
-            return name; // returns name's value to this method's caller
+            get // returns the corresponding instance variable's value
+            {
+                return name;
+            }
+            set // assigns a new value to the corresponding instace variable
+            {
+                name = value; // value is implicitly declared and initialized
+            }
         }
     }
 }
--- AccountTest/Program.cs
+++ AccountTest/Program.cs
@@ -6,9 +6,19 @@ namespace AccountTest
     {
         private static void Main(string[] args)
         {
+            // create an Account object and assign it to myAccount
             Account myAccount = new Account();
-            myAccount.SetName("Chance-100");
-            Console.WriteLine($"You are viewing account: {myAccount.GetName()}");
+
+            // display myAccount's initial name (there isn't one yet)
+            Console.WriteLine($"Initial name is {myAccount.Name}");
+
+            // prompt for and read the name, then put the name in the object
+            Console.Write("Enter the name: "); // prompt
+            string theName = Console.ReadLine(); // read the name
+            myAccount.Name = theName; // put theName in the myAccount object
+
+            // display the name stored in the myAccount object
+            Console.WriteLine($"You are viewing account: {myAccount.Name}");
         }
     }
 }
